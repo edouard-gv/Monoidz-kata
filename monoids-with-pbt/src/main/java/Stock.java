7526ff738663b917e1bb2b6658d37a0c0de21cd6
Édouard <edouard@gomez-vaez.com>
2021-01-19 18:52:13 +0100

    type in maven group name + renaming internal stock into 'quantity'

--- monoids-with-pbt/src/main/java/Stock.java
+++ monoids-with-pbt/src/main/java/Stock.java
@@ -3,14 +3,14 @@ import java.util.Objects;
 public class Stock {
 
     public static final Stock NEUTRAL = new Stock(0);
-    private int stock;
+    private int quantity;
 
-    public Stock(int stock) {
-        this.stock = stock;
+    public Stock(int quantity) {
+        this.quantity = quantity;
     }
 
     public Stock combine(Stock stock) {
-        return new Stock(this.stock + stock.stock);
+        return new Stock(this.quantity + stock.quantity);
     }
 
     @Override
@@ -18,18 +18,18 @@ public class Stock {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Stock stock1 = (Stock) o;
-        return stock == stock1.stock;
+        return quantity == stock1.quantity;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(stock);
+        return Objects.hash(quantity);
     }
 
     @Override
     public String toString() {
         return "Stock{" +
-                "stock=" + stock +
+                "stock=" + quantity +
                 '}';
     }
 }
